254b33ea1ef0413bd0dc010c588fd47161f89913
phanimadiraju19 <phanimadiraju19@gmail.com>
2019-10-16 11:47:34 +0530

    Styled Header

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -5,7 +5,13 @@ import Button from '@material-ui/core/Button';
 class Header extends Component {
     render() {
         return (
-            <div><Button variant="contained" color="default">Login</Button></div>
+            <div>
+                <header className="app-header">
+                    <div className="login-button">
+                        <Button variant="contained" color="default">Login</Button>
+                    </div>
+                </header>
+            </div>
         )
     }
 }
