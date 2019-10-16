ad06032b7c6932630306f6d161fa3f8873564a93
phanimadiraju19 <phanimadiraju19@gmail.com>
2019-10-16 17:50:36 +0530

    Added 'required' field validation in password control in login form && Fixed issues related to state in modal for login & register

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -44,12 +44,20 @@ class Header extends Component {
             modalIsOpen: false,
             value : 0 ,
             usernameRequired: "dispNone",
-            username: ""
+            username: "",
+            passwordRequired: "dispNone",
+            password: ""
         }
 
     }
     openModalHandler = () => {
-        this.setState({ modalIsOpen: true });
+        this.setState({ modalIsOpen: true,
+            value: 0,
+            usernameRequired: "dispNone",
+            username: "" ,
+            passwordRequired: "dispNone",
+            password: ""
+        });
     }
     closeModalHandler = () => {
         this.setState({ modalIsOpen: false });
@@ -59,11 +67,16 @@ class Header extends Component {
     }
     loginClickHandler = () => {
         this.state.username === "" ? this.setState({ usernameRequired: "dispBlock" }) : this.setState({ usernameRequired: "dispNone" });
+        this.state.password === "" ? this.setState({ passwordRequired: "dispBlock" }) : this.setState({ passwordRequired: "dispNone" });
     }
 
     inputUsernameChangeHandler = (e) => {
         this.setState({ username: e.target.value });
     }
+    inputPasswordChangeHandler = (e) => {
+        this.setState({ password: e.target.value });
+    }
+
 
     render() {
         return (
@@ -98,7 +111,10 @@ class Header extends Component {
 
                         <FormControl required>
                             <InputLabel htmlFor="password">Password</InputLabel>
-                            <Input id="password" type="password" />
+                            <Input id="password" type="password" password={this.state.password} onChange={this.inputPasswordChangeHandler} />
+                                <FormHelperText className={this.state.passwordRequired}>
+                                    <span className="red">required</span>
+                                </FormHelperText>
                         </FormControl><br/><br/>
 
                         <Button variant="contained" color="primary"  onClick={this.loginClickHandler}>LOGIN</Button>
